3d87baceb33d9cb4baa83766dc5b161b4d2fc7e2
João Paulo Cabral <jpocabral@gmail.com>
2021-04-12 19:55:26 -0300

    adicionando a estrutura das atividades e uma listagem parcial das atividades cadastradas

--- kanban/atividade.py
+++ kanban/atividade.py
@@ -1,2 +1,12 @@
 #definir aqui a estrutura de uma atividade
 #Contem: nome, descrição, data, e uma lista de reponsáveis, e uma coluna
+
+from collections import namedtuple
+
+
+
+Atividade = namedtuple("Atividade", "Nome Descricao Data Lista_de_responsaveis Coluna")
+
+def criarAtividade(nome, descricao, data, lista_de_responsaveis, coluna):
+    atividade = Atividade(nome, descricao, data, lista_de_responsaveis, coluna)
+    return atividade
--- kanban/main.py
+++ kanban/main.py
@@ -1,29 +1,50 @@
-import atividade,coluna,responsavel
+from typing import List
+
+from atividade import *
+from atividade import Atividade
+from responsavel import *
+from coluna import *
 
 def menu():
-    print('\n1.Adicionar uma atividade\n2.Cadastrar um responsável\n3.Atribuir atividades'
-        '\n4.Listar atividades de um responsável\n5.Cadastrar coluna\n6.Atribuir atividade a uma coluna\n7.Sair\n')
+    print('\n1.Cadastrar uma atividade\n2.Listar todas as atividades\n3.Cadastrar um responsável\n4.Atribuir atividades'
+        '\n5.Listar atividades de um responsável\n6.Cadastrar coluna\n7.Atribuir atividade a uma coluna\n8.Sair\n')
 
 if __name__ == '__main__':
-    print('Bem vindo ao Kanban"')
+    print('Bem vindo ao Kanban!')
+
+    lista_de_atividades: List[Atividade] = []
 
     opcao = True
     while opcao:
         menu()
         opcao = input("Selecione uma opção:")
         if opcao == "1":
-            print("\n Adicionando uma atividade...")
+            nome = input("Nome:")
+            descricao = input("Descrição:")
+            data = input("Data:")
+            lista_de_responsaveis = False
+            coluna = False
+            lista_de_atividades.append(criarAtividade(nome, descricao, data, lista_de_responsaveis, coluna))
+            print("\n"+str(len(lista_de_atividades))+"ª atividade criada com sucesso!")
         elif opcao == "2":
-            print("\n Cadastrando um responsável...")
+            for atividade in lista_de_atividades:
+                print("\n------------------------------------")
+                print("Nome:",atividade.Nome)
+                print("Descrição:",atividade.Descricao)
+                print("Data:", atividade.Data)
+                print("Responsáveis:", atividade.Lista_de_responsaveis)
+                print("Coluna:", atividade.Coluna)
         elif opcao == "3":
-            print("\n Atribuindo atividades...")
+            print("\n Cadastrando um responsável...")
         elif opcao == "4":
-            print("\n Listando atividades de um determinado responsável...")
+            print("\n Atribuindo atividades...")
         elif opcao == "5":
-            print("\n Cadastrando uma coluna...")
+            print("\n Listando atividades de um determinado responsável...")
         elif opcao == "6":
-            print("\n Atribuindo atividade a uma coluna...")
+            print("\n Cadastrando uma coluna...")
         elif opcao == "7":
+            print("\n Atribuindo atividade a uma coluna...")
+        elif opcao == "8":
             print("\n Até mais!")
             opcao = False
         elif opcao != "":
